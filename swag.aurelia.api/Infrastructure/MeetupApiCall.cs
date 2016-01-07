362126080c836999adcd36a41674236cd082eb74
Richard Wilde <richard@wildesoft.net>
2016-01-07 19:06:47 +0000

    Add restsharp

---
+++ swag.aurelia.api/Infrastructure/MeetupApiCall.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using RestSharp;
+
+namespace swag.aurelia.api.Infrastructure
+{
+    public class MeetupApiCall
+    {
+        private readonly string meetupApiKey;
+        readonly Uri baseUrl = new Uri("https://api.meetup.com/2");
+
+        public MeetupApiCall(string meetupApiKey)
+        {
+            this.meetupApiKey = meetupApiKey;
+        }
+
+        public T Execute<T>(RestRequest request) where T : new()
+        {
+            var client = new RestClient { BaseUrl = baseUrl };
+            request.AddUrlSegment("key", meetupApiKey);
+
+            var response = client.Execute<T>(request);
+
+            //TODO Not checking for response.ErrorException != null
+
+            return response.Data;
+        }
+
+    }
+}
